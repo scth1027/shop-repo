6b8a73a85021111e3cedd9dda2027719a1a996cc
Thiemo Schäfer <scth1027 (at) hs-karlsruhe.de>
2013-10-28 20:41:52 +0100

    Klasse Firmenkunde

---
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Firmenkunde.java
@@ -0,0 +1,61 @@
+package de.shop.kundenverwaltung.domain;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import de.shop.bestellverwaltung.domain.Bestellung;
+
+@XmlRootElement
+public class Firmenkunde extends AbstractKunde
+{
+	//Attribute
+	private String firmenname;
+	private String ansprechpartner;
+	
+	//Getter & Setter
+	public String getFirmenname() 
+	{
+		return firmenname;
+	}
+	
+	public void setFirmenname(String firmenname)
+	{
+		this.firmenname = firmenname;
+	}
+
+	public String getAnsprechpartner() 
+	{
+		return ansprechpartner;
+	}
+
+	public void setAnsprechpartner(String ansprechpartner) 
+	{
+		this.ansprechpartner = ansprechpartner;
+	}
+	
+	//Konstruktor
+	public Firmenkunde(Long id, String email, Adresse adresse,
+			List<Bestellung> bestellungen, URI bestellungenUri,
+			String firmenname, String ansprechpartner) {
+		super(id, email, adresse, bestellungen, bestellungenUri);
+		this.firmenname = firmenname;
+		this.ansprechpartner = ansprechpartner;
+	}
+
+	//Methoden
+	@Override
+	public String toString() {
+		return "Firmenkunde [firmenname=" + firmenname + ", ansprechpartner="
+				+ ansprechpartner +  super.toString() +"]";
+	}
+	
+
+	
+	
+	
+	
+	
+
+}
