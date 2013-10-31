a138866342b2d93b66b190078137b1d0c1636725
Tobias Pfefferle <Tobi@10.116.26.66>
2013-10-31 10:25:13 +0100

    Rest Infrastruktur erstellt

--- shop/src/main/java/de/shop/kundenverwaltung/rest/KundeResource.java
+++ shop/src/main/java/de/shop/kundenverwaltung/rest/KundeResource.java
@@ -1,7 +1,18 @@
 package de.shop.kundenverwaltung.rest;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.APPLICATION_XML;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 
+
+@Path("/kunden")
+@Produces({APPLICATION_JSON, APPLICATION_XML, TEXT_PLAIN} )
+@Consumes
 public class KundeResource {
 	
 }
