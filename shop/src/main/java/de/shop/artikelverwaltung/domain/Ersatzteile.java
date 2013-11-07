3880061a37e2ede7758b68f3077b8fd202920ca6
Vitalij Faut <favi1011@hs-karlsruhe.de>
2013-11-07 13:29:52 +0100

    Ersatzteile angelegt

--- shop/src/main/java/de/shop/artikelverwaltung/domain/Ersatzteile.java
+++ shop/src/main/java/de/shop/artikelverwaltung/domain/Ersatzteile.java
@@ -1,5 +1,29 @@
 package de.shop.artikelverwaltung.domain;
 
-public class Ersatzteile {
+import javax.xml.bind.annotation.XmlRootElement;
 
+
+@XmlRootElement
+
+public class Ersatzteile {
+	
+	private static final long serialVersionUID = 1L;
+
+	private String artikeldetails;
+
+	public String getArtikeldetails() {
+		return artikeldetails;
+	}
+
+	public void setArtikeldetails(String artikeldetails) {
+		this.artikeldetails = artikeldetails;
+		
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Ersatzteile [" + super.toString() + ", Artikeldetails:" + artikeldetails + "]";
+	}
+	
 }
