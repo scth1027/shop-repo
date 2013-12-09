e14a93cba6f7eb4139d07f446e39e6fba1886690
Thiemo Schäfer <scth1027 (at) hs-karlsruhe.de>
2013-12-09 16:56:00 +0100

    Merge

--- shop/src/main/java/de/shop/util/rest/ShopExceptionMapper.java
+++ shop/src/main/java/de/shop/util/rest/ShopExceptionMapper.java
@@ -15,9 +15,6 @@ import de.shop.util.AbstractShopException;
 import de.shop.util.interceptor.Log;
 
 
-/**
- * @author <a href="mailto:Juergen.Zimmermann@HS-Karlsruhe.de">J&uuml;rgen Zimmermann</a>
- */
 @Provider
 @ApplicationScoped
 @Log
