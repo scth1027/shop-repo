3a43beaf1ebf473497c16bf051fd9413693d7e16
Thiemo Schäfer <scth1027 (at) hs-karlsruhe.de>
2013-12-09 14:12:55 +0100

    Methoden KundenService

--- shop/src/main/java/de/shop/kundenverwaltung/service/KundenService.java
+++ shop/src/main/java/de/shop/kundenverwaltung/service/KundenService.java
@@ -1,5 +1,69 @@
 package de.shop.kundenverwaltung.service;
 
-public class KundenService {
+import java.io.Serializable;
+import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import de.shop.kundenverwaltung.domain.AbstractKunde;
+import de.shop.util.Mock;
+
+public class KundenService implements Serializable {
+
+	private static final long serialVersionUID = 5326204988915561298L;
+
+	@NotNull(message = "{kunde.notFound.id}")
+	public AbstractKunde findKundeById(Long id) {
+		if (id == null) {
+			return null;
+		}
+		return Mock.findKundeById(id);
+	}
+	
+	@Size(min = 1, message = "{kunde.notFound.nachname}")
+	public List<AbstractKunde> findKundenByNachname(String nachname) {
+
+		return Mock.findKundenByNachname(nachname);
+	}
+	
+	public List<AbstractKunde> findAllKunden() {
+
+		return Mock.findAllKunden();
+	}
+	
+	public <T extends AbstractKunde> T createKunde(T kunde) {
+		if (kunde == null) {
+			return kunde;
+		}
+
+		kunde = Mock.createKunde(kunde);
+
+		return kunde;
+	}
+	
+	public <T extends AbstractKunde> T updateKunde(T kunde) {
+		if (kunde == null) {
+			return null;
+		}
+		
+		Mock.updateKunde(kunde);
+		
+		return kunde;
+	}
+	
+	public void deleteKunde(Long kundeId) {
+		AbstractKunde kunde = findKundeById(kundeId); 
+		if (kunde == null) {
+			return;
+		}
+		
+		Mock.deleteKunde(kundeId);
+	}
+
+	
 }
+	
+	
+
+
